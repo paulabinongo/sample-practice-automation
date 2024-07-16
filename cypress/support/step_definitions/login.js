9c0d2ec18915eabd0ad75581c92d5909e7494f78
Paula <paula.binongo@98labs.com>
2024-07-16 20:28:56 +0800

    Modify the link or website to be used as a playground

--- cypress/support/step_definitions/login.js
+++ cypress/support/step_definitions/login.js
@@ -1,5 +1,5 @@
 import { Given } from 'cypress-cucumber-preprocessor/steps'
 
 Given('I open the website', () => {
-    cy.visit('https://experience-api-dca-processing-r2-4cb4n4kxsq-as.a.run.app/login'); // Replace with the URL of your website
+    cy.visit('https://www.saucedemo.com/'); // Replace with the URL of your website
 });
