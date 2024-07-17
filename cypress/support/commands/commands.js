cadd6124a1493406ba3e6f66d46a0c609af2a207
Paula <paula.binongo@98labs.com>
2024-07-17 13:18:18 +0800

    Modified the code, restructure the files

--- cypress/support/commands/commands.js
+++ cypress/support/commands/commands.js
@@ -1,20 +1,21 @@
 const faker = require('faker');
 
-Cypress.Commands.add('generateAndSaveUserData', () => {
+Cypress.Commands.add('generateDataAndSaveData', () => {
     return cy.readFile('cypress/fixtures/generated-test-data.json').then((existingData) => {
         const userData = {
             firstName: faker.name.firstName(),
             lastName: faker.name.lastName(),
             email: faker.internet.email(),
-        };
+        }
 
-        // Append new data to existingData
-        const newData = [...existingData, userData];
+        // Append new data to the existing data
+        const newData = [...existingData, userData]
 
         // Write the updated data back to the fixture file
         return cy.writeFile('cypress/fixtures/generated-test-data.json', newData).then(() => {
-            // Return both userData and newData as an object
+
+            // Return both the user data and new data as an object
             return { userData, newData };
-        });
-    });
-});
+        })
+    })
+})
--- cypress/support/step_definitions/signup.js
+++ cypress/support/step_definitions/signup.js
@@ -1,25 +1,22 @@
-import { Given, When, And } from 'cypress-cucumber-preprocessor/steps'
-import { generateAndSaveUserData } from '../commands/commands'
+import { Given, When, And } from 'cypress-cucumber-preprocessor/steps';
+import { generateDataAndSaveData } from '../commands/commands';
 
 before(() => {
-    cy.viewport(Cypress.config('viewportWidth'), Cypress.config('viewportHeight'));
-})
-
+    cy.viewport(Cypress.config('viewportHeight'), Cypress.config('viewportWidth'));
+});
 
 Given('I open the website to signup', () => {
     const signupUrl = Cypress.config('baseUrl');
-    cy.visit(`${signupUrl}/signup`)
+    cy.visit(`${signupUrl}/signup`);
 });
 
-
 When('I sign up', () => {
-    cy.generateAndSaveUserData().as('userData');
-    cy.get('@userData').then(({ userData, newData }) => {
+    cy.generateDataAndSaveData().as('userData');
+    cy.get('@userData').then(({ userData }) => {
         // Use userData and newData as needed
         const fullName = `${userData.firstName} ${userData.lastName}`;
         // Fill out the signup form with generated data
         cy.get('[data-qa="signup-name"]').type(fullName);
         cy.get('[data-qa="signup-email"]').type(userData.email);
-
     });
-})
+});
