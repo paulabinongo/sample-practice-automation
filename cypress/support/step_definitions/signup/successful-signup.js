c86bf166a0b0bfd83507712ba87c0375f93662e0
paula.binongo <paula.binongo@98labs.com>
2024-07-26 01:30:40 +0800

    Refactore the code

--- cypress/support/step_definitions/signup/successful-signup.js
+++ cypress/support/step_definitions/signup/successful-signup.js
@@ -1,8 +1,8 @@
 import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps';
-import { generateDataAndSaveData } from '../../commands/commands'
+import { generateDataAndSaveData } from '../../commands/commands';
 
 before(() => {
-    cy.viewport(Cypress.config('viewportHeight'), Cypress.config('viewportWidth'));
+    cy.viewport(Cypress.config('viewportWidth'), Cypress.config('viewportHeight'));
 });
 
 Given('that I am on the sign up page', () => {
@@ -13,99 +13,63 @@ Given('that I am on the sign up page', () => {
 When('I fill out the sign up form with valid data', () => {
     cy.generateDataAndSaveData().as('userData');
     cy.get('@userData').then(({ userData }) => {
-
-        // Use userData and newData as needed
         const fullName = `${userData.firstName} ${userData.lastName}`;
 
         // Fill out the signup form with generated data
         cy.get('[data-qa="signup-name"]').type(fullName);
         cy.get('[data-qa="signup-email"]').type(userData.email);
     });
+});
 
-    And('I submit the Sign Up Form', () => {
-        cy.get('[data-qa="signup-button"]').click();
-    })
+And('I submit the Sign Up Form', () => {
+    cy.get('[data-qa="signup-button"]').click();
+});
 
-    And('I should be able to view the form for the Account Information and Address Information', () => {
-        cy.get(':nth-child(1) > b').contains('Enter Account Information', { matchCase: false })
-            .get('form > .title > b').contains('Address Information', { matchCase: false })
-            .wait(3500);
-    })
+And('I should be able to view the form for the Account Information and Address Information', () => {
+    cy.get(':nth-child(1) > b').contains('Enter Account Information', { matchCase: false });
+    cy.get('form > .title > b').contains('Address Information', { matchCase: false });
+    cy.wait(3500);
+});
 
-    And('I should be able to add my Personal Details on each section', () => {
-        cy.generateDataAndSaveData().as('gender');
-        cy.get('@gender').then(({ userData }) => {
-
-            // Select gender radio button based on generated gender
-            if (userData.gender === 'Mr.') {
-                cy.get('#id_gender1').check({ force: true }); // Adjust selector as per your application
-            } else if (userData.gender === 'Mrs.') {
-                cy.get('#id_gender2').check({ force: true }); // Adjust selector as per your application
-            }
-        });
-        cy.get('[data-qa="password"]').type('Password123');
-        cy.get('[data-qa="days"]').then(select => {
-            const dayOption = select.find('option');
-            const dayOptionCount = dayOption.length;
-            const dayRandomIndex = Math.floor(Math.random() * dayOptionCount);
-            const dayRandomValue = dayOption[dayRandomIndex].value;
-
-            //Wrap the select element with Cypress and select the random option by its value
-            cy.wrap(select).select(dayRandomValue);
-        })
-
-        cy.get('[data-qa="months"]').then(select => {
-            const monthOptions = select.find('option');
-            const monthOptionCount = monthOptions.length;
-            const monthRandomIndex = Math.floor(Math.random() * monthOptionCount);
-            const monthRandomValue = monthOptions[monthRandomIndex].value;
-
-            //Wrap the select element with Cypress and select the random option by its value
-            cy.get(select).select(monthRandomValue);
-        })
-
-        cy.get('[data-qa="years"]').then(select => {
-            const yearOption = select.find('option');
-            const yearOptionCount = yearOption.length;
-            const yearRandomIndex = Math.floor(Math.random() * yearOptionCount);
-            const yearRandomValue = yearOption[yearRandomIndex].value;
-
-            //Wrap the select element with Cypress and select the random option by its value
-            cy.wrap(select).select(yearRandomValue)
-        })
-
-        cy.get('#newsletter').each(($checkbox) => {
-            //Generate a randomBoolean
-            const randomBoolean = Math.random() >= 0.5;
-            if (randomBoolean) {
-                //Check the checkbox if it's not already checked
-                cy.wrap($checkbox).check();
-            } else {
-                //Uncheck the checkbox if it's already uncheck
-                cy.wrap($checkbox).uncheck();
-            }
-        })
-        cy.get('#newsletter').each(($checkbox) => {
-            const isChecked = $checkbox.prop('checked');
-            cy.wrap($checkbox).should(isChecked ? 'be.checked' : 'not.be.checked');
-        })
-
-        cy.get('#optin').each(($checkbox) => {
-            // Generate a random boolean
-            const randomBoolean = Math.random() >= 0.2;
-
-            if (randomBoolean) {
-                // Check the checkbox if it's not already checked
-                cy.wrap($checkbox).check();
-            } else {
-                // Uncheck the checkbox if it's not already unchecked
-                cy.wrap($checkbox).uncheck();
-            }
-        })
-        cy.get('#optin').each(($checkbox) => {
-            const isChecked = $checkbox.prop('checked');
-            cy.wrap($checkbox).should(isChecked ? 'be.checked' : 'not.be.checked')
-        });
+And('I should be able to add my Personal Details on each section', () => {
+    cy.generateDataAndSaveData().as('gender');
+    cy.get('@gender').then(({ userData }) => {
+        // Select gender radio button based on generated gender
+        if (userData.gender === 'Mr.') {
+            cy.get('#id_gender1').check({ force: true });
+        } else if (userData.gender === 'Mrs.') {
+            cy.get('#id_gender2').check({ force: true });
+        }
     });
 
+    cy.get('[data-qa="password"]').type('Password123');
+
+    // Function to select a random option from a dropdown
+    const selectRandomOption = (selector) => {
+        cy.get(selector).then($select => {
+            const options = $select.find('option');
+            const randomIndex = Math.floor(Math.random() * options.length);
+            const randomValue = options.eq(randomIndex).val();
+            cy.wrap($select).select(randomValue);
+        });
+    };
+
+    selectRandomOption('[data-qa="days"]');
+    selectRandomOption('[data-qa="months"]');
+    selectRandomOption('[data-qa="years"]');
+
+    // Randomly check/uncheck checkboxes
+    const toggleCheckbox = (selector) => {
+        cy.get(selector).each($checkbox => {
+            const randomBoolean = Math.random() >= 0.5;
+            if (randomBoolean) {
+                cy.wrap($checkbox).check({ force: true });
+            } else {
+                cy.wrap($checkbox).uncheck({ force: true });
+            }
+        });
+    };
+
+    toggleCheckbox('#newsletter');
+    toggleCheckbox('#optin');
 });
